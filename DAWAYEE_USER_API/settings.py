74f730a11bb2b495a1ca178c7a8dd10c248a8e01
Arya_prakash <aryaprakash0406@gmail.com>
2022-06-27 17:46:28 +0530

    h

--- DAWAYEE_USER_API/settings.py
+++ DAWAYEE_USER_API/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_zk=9yl=ul!po@_z-)wq&x4=&!(ons2(pogyu52+f7f37v3j$y'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG",False))
+DEBUG = True
+# DEBUG = bool(os.environ.get("DEBUG",False))
 
 ALLOWED_HOSTS = ['*']
 
