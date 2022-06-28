159d0add419b950fef093e39015e6fe0c93b006b
Arya_prakash <aryaprakash0406@gmail.com>
2022-06-28 09:32:03 +0530

    postgresql added

--- DAWAYEE_USER_API/settings.py
+++ DAWAYEE_USER_API/settings.py
@@ -83,11 +83,22 @@ WSGI_APPLICATION = 'DAWAYEE_USER_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql',
+      'NAME':'User_database',
+      'USER':'postgres',
+      'PASSWORD':'Arya@123',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 # JWT Configuration
