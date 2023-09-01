99dcfa5da96a0891fcadd77256577b1fdc7d852a
Arya Prakash <73129490+aryaprakash0406@users.noreply.github.com>
2023-09-01 18:30:11 +0530

    yvy

--- DAWAYEE_USER_API/settings.py
+++ DAWAYEE_USER_API/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'DAWAYEE_USER_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES={
-#    'default':{
-#       'ENGINE':'django.db.backends.postgresql',
-#       'NAME':'user_database',
-#       'USER':'oculusvertex',
-#       'PASSWORD':'arya1234',
-#       'HOST':'userdatabaseinstance.cwninlemtkwt.ap-south-1.rds.amazonaws.com',
-#       'PORT':'5432',
-#    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql',
+      'NAME':'user_database',
+      'USER':'oculusvertex',
+      'PASSWORD':'arya1234',
+      'HOST':'userdatabaseinstance.cwninlemtkwt.ap-south-1.rds.amazonaws.com',
+      'PORT':'5432',
+   }
+}
 
 # JWT Configuration
 REST_FRAMEWORK = {
